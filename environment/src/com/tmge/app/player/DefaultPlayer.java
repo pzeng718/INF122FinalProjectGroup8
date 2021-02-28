8d5a02e8b20b659b04d2209956bcdf20052216d7
dongpc21 <dongpc@uci.edu>
2021-02-27 16:29:40 -0800

    Update DefaultPlayer.java

--- environment/src/com/tmge/app/player/DefaultPlayer.java
+++ environment/src/com/tmge/app/player/DefaultPlayer.java
@@ -25,6 +25,43 @@ public class DefaultPlayer {
     @SerializedName("username")
     private final String username = "";
 
+    @Builder.Default
+    @SerializedName("password")
+    private final String password = "";
+
+    @Builder.Default
+    @SerializedName("level")
+    private int level = 1;
+
+    @Builder.Default
+    @SerializedName("score")
+    private int score = 0;
+
+    @Builder.Default
+    @SerializedName("lives")
+    private int lives = 0; // needs changed
+
+
+    public void addLives(int l) {
+        lives += l;
+    }
+    public void removeLives(int l) {
+        lives -= l;
+    }
+
+    public void addScore(int s) {
+        score += s;
+    }
+
+    public void incrementLevel() {
+        level++;
+    }
+
+    public boolean login() {
+        return (username == "" && password == ""); // change this
+        // checks database to see if username and password matches
+    }
+
     @Override
     public String toString() {
         return getUsername();
